221d358d47d3fa9bbb2a2e23b00bc5270aaaddc2
kwongio <rldh11111@naver.com>
2024-12-11 18:20:07 +0900

    refactor: Pessimistic Lock을 사용한 동시성 문제 해결

--- src/main/java/com/gio/couponsystem/conpon/repository/CouponRepository.java
+++ src/main/java/com/gio/couponsystem/conpon/repository/CouponRepository.java
@@ -1,7 +1,17 @@
 package com.gio.couponsystem.conpon.repository;
 
 import com.gio.couponsystem.conpon.domain.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select c from Coupon c where c.id = :id")
+    Optional<Coupon> findByIdWithLock(@Param("id") Long id);
 }
--- src/main/java/com/gio/couponsystem/conpon/service/CouponService.java
+++ src/main/java/com/gio/couponsystem/conpon/service/CouponService.java
@@ -33,16 +33,10 @@ public class CouponService {
                 .orElseThrow(() -> new CustomException(ExceptionCode.COUPON_NOT_FOUND));
     }
 
+    @Transactional
     public void assignCoupon(Long couponId) {
-        ReentrantLock lock = locks.computeIfAbsent(couponId, k -> new ReentrantLock());
-        lock.lock();
-        try {
-            Coupon coupon = couponRepository.findById(couponId)
-                    .orElseThrow(() -> new CustomException(ExceptionCode.COUPON_NOT_FOUND));
-            coupon.assign();
-            couponRepository.save(coupon);
-        } finally {
-            lock.unlock();
-        }
+        Coupon coupon = couponRepository.findByIdWithLock(couponId)
+                .orElseThrow(() -> new CustomException(ExceptionCode.COUPON_NOT_FOUND));
+        coupon.assign();
     }
 }

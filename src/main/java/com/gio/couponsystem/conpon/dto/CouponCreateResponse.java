75bff7e1c18fa1adab6c378fff34052580630981
kwongio <rldh11111@naver.com>
2024-12-11 14:19:59 +0900

    feat: 쿠폰 생성 response swagger 문서화

--- src/main/java/com/gio/couponsystem/conpon/dto/CouponCreateResponse.java
+++ src/main/java/com/gio/couponsystem/conpon/dto/CouponCreateResponse.java
@@ -7,11 +7,20 @@ import java.time.LocalDateTime;
 
 @Schema(description = "쿠폰 생성 응답 데이터") // 클래스 수준에서 설명 추가
 public record CouponCreateResponse(
-        @Schema(description = "쿠폰 ID", example = "1") Long id,
-        @Schema(description = "쿠폰 제목", example = "10% 할인 쿠폰") String title,
-        @Schema(description = "쿠폰 수량", example = "100") long quantity,
-        @Schema(description = "쿠폰 시작 날짜", example = "2023-12-01T00:00:00") LocalDateTime startDate,
-        @Schema(description = "쿠폰 종료 날짜", example = "2023-12-31T23:59:59") LocalDateTime endDate
+        @Schema(description = "쿠폰 ID", example = "1")
+        Long id,
+
+        @Schema(description = "쿠폰 제목", example = "10% 할인 쿠폰")
+        String title,
+
+        @Schema(description = "쿠폰 수량", example = "100")
+        long quantity,
+
+        @Schema(description = "쿠폰 시작 날짜", example = "2023-12-01T00:00:00")
+        LocalDateTime startDate,
+
+        @Schema(description = "쿠폰 종료 날짜", example = "2023-12-31T23:59:59")
+        LocalDateTime endDate
 ) {
     public static CouponCreateResponse from(Coupon coupon) {
         return new CouponCreateResponse(

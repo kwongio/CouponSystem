6b8681e5efe7e9be1f6334268e1c8ea9a82bc3e2
kwongio <rldh11111@naver.com>
2024-12-15 16:54:52 +0900

    feat: Producer 추가

---
+++ src/main/java/com/gio/couponsystem/conpon/repository/CouponAssignRequest.java
@@ -0,0 +1,12 @@
+package com.gio.couponsystem.conpon.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CouponAssignRequest {
+    private Long couponId;
+}
--- src/main/java/com/gio/couponsystem/conpon/service/CouponService.java
+++ src/main/java/com/gio/couponsystem/conpon/service/CouponService.java
@@ -2,6 +2,8 @@ package com.gio.couponsystem.conpon.service;
 
 import com.gio.couponsystem.conpon.domain.Coupon;
 import com.gio.couponsystem.conpon.dto.CouponCreateRequest;
+import com.gio.couponsystem.conpon.repository.CouponAssignRequest;
+import com.gio.couponsystem.conpon.repository.CouponProducer;
 import com.gio.couponsystem.conpon.repository.CouponRepository;
 import com.gio.couponsystem.conpon.validator.CouponValidator;
 import com.gio.couponsystem.exception.CustomException;
@@ -17,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CouponService {
     private final CouponRepository couponRepository;
     private final CouponValidator couponValidator;
+    private final CouponProducer couponProducer;
 
     @Transactional
     public Coupon create(CouponCreateRequest request) {
@@ -30,18 +33,11 @@ public class CouponService {
                 .orElseThrow(() -> new CustomException(ExceptionCode.COUPON_NOT_FOUND));
     }
 
-
-    @Transactional
     public void assignCoupon(Long couponId) {
-        int updatedRows = couponRepository.decrementCouponQuantity(couponId);
-        if (updatedRows == 0) {
-            throw new CustomException(ExceptionCode.COUPON_OUT_OF_STOCK);
-        }
+        couponProducer.sendAssignCouponRequest(new CouponAssignRequest(couponId));
     }
-//    @Transactional
-//    public void assignCoupon(Long couponId) {
-//        Coupon coupon = couponRepository.findByIdWithLock(couponId)
-//                .orElseThrow(() -> new CustomException(ExceptionCode.COUPON_NOT_FOUND));
-//        coupon.assign();
-//    }
+
+    private boolean isOutOfStock(int remainingCoupons) {
+        return remainingCoupons <= 0;
+    }
 }

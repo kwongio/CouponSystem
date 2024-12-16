be97c8e58760aee08968b0754c6d73c390188088
kwongio <rldh11111@naver.com>
2024-12-16 13:02:46 +0900

    refactor: kafka producer 설정 yml에서 코드로 변경

---
+++ src/main/java/com/gio/couponsystem/config/KafkaProducerConfig.java
@@ -0,0 +1,48 @@
+package com.gio.couponsystem.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public DefaultKafkaProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        // Kafka broker 정보
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+
+        // 직렬화 설정
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 데이터 유실 방지 설정
+        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true); // 멱등성 활성화
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all"); // 모든 복제 완료 후 확인
+
+        // 배치 설정
+        configProps.put(ProducerConfig.LINGER_MS_CONFIG, 1000); // 최대 10ms 대기
+        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 16 * 1024); // 16KB 배치 크기
+
+        // 재시도 및 타임아웃 설정
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 5); // 실패 시 최대 5번 재시도
+        configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000); // 요청 타임아웃 30초
+
+        // 압축 설정 (선택 사항)
+//        configProps.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip"); // gzip으로 압축
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}

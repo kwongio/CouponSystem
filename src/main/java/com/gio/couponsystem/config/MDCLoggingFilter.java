e8d0d950c92b1b48352073b034293ebdfc0b24a9
kwongio <rldh11111@naver.com>
2024-12-17 17:14:52 +0900

    feat: MDC 적용

---
+++ src/main/java/com/gio/couponsystem/config/MDCLoggingFilter.java
@@ -0,0 +1,25 @@
+package com.gio.couponsystem.config;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class MDCLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        MDC.put("traceId", "[" + UUID.randomUUID() + "]");
+        filterChain.doFilter(servletRequest, servletResponse);
+        MDC.clear();
+    }
+}

d69f7cec59ee5647d8ea5a190eae0b87ea763a6d
kwongio <rldh11111@naver.com>
2024-12-18 16:40:16 +0900

    feat: prometheus 카프카 모니터링 추가

--- src/main/java/com/gio/couponsystem/config/KafkaTopicConfig.java
+++ src/main/java/com/gio/couponsystem/config/KafkaTopicConfig.java
@@ -1,12 +1,20 @@
 package com.gio.couponsystem.config;
 
+import java.util.Map;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 
 @Configuration
 public class KafkaTopicConfig {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = Map.of("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
+        return new KafkaAdmin(configs);
+    }
 
     @Bean
     public NewTopic couponAssignTopic() {
